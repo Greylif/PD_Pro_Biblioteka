c87ecacc4e00969a25a3fad0e855855c634f1a2b
Oskar Andrzejewski <oskar.andrzejewski@onet.pl>
2025-03-20 16:32:23 +0100

    Project setup

---
+++ PD_Pro_Biblioteka/src/main/groovy/com/example/pd_pro_biblioteka/PdProBibliotekaApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.pd_pro_biblioteka
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class PdProBibliotekaApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(PdProBibliotekaApplication, args)
+    }
+
+}
